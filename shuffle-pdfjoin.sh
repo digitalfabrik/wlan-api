194a88334be2f69023e73d89aa9fe7ab9b96fbfa
Max Ammann <maximilian.ammann@googlemail.com>
2018-10-18 17:43:21 +0200

    Support ads with only 1 page

--- shuffle-pdfjoin.sh
+++ shuffle-pdfjoin.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
-VOUCHER_PDF=vouchers_tatdf_roll24.csv.pdf
-VOUCHER_PDF_PAGES=200
-ADS_PDF=ads.pdf
-ADS_PDF_PAGES=4
+VOUCHER_PDF=vouchers_tatdf_roll24.csv-2x2.pdf
+VOUCHER_PDF_PAGES=50
+ADS_PDF=ads-2x2.pdf
+ADS_PDF_PAGES=1
 
 PAGES=()
 for k in $(seq 1 ${VOUCHER_PDF_PAGES}); do
     PAGES+=($VOUCHER_PDF);
     PAGES+=($k);
     PAGES+=($ADS_PDF);
-    PAGES+=($(($k % ($ADS_PDF_PAGES - 1) + 1)));
+    AD_PAGE_MODULO=$(($ADS_PDF_PAGES - 1))
+    if [ "$AD_PAGE_MODULO" -eq "0" ]; then
+        PAGES+=(1);
+    else
+        PAGES+=($(($k % $AD_PAGE + 1)));
+    fi
 done
 
 pdfjoin ${PAGES[@]} --outfile shuffled.pdf --paper a5paper --rotateoversize false
