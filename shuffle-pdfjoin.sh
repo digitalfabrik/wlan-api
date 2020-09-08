73f88bcd478f78d25627fab1f6eb4a88d5db0230
Maximilian Ammann <max@maxammann.org>
2020-09-08 15:04:44 +0200

    Add separate generate.py

--- shuffle-pdfjoin.sh
+++ shuffle-pdfjoin.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
-VOUCHER_PDF=$1
-VOUCHER_PDF_PAGES=50
-ADS_PDF=$2
-ADS_PDF_PAGES=1
+VOUCHER_PDF=$2
+VOUCHER_PDF_PAGES=$3
+ADS_PDF=$1
+ADS_PDF_PAGE=1
 
 PAGES=()
 for k in $(seq 1 ${VOUCHER_PDF_PAGES}); do
     PAGES+=($VOUCHER_PDF);
     PAGES+=($k);
     PAGES+=($ADS_PDF);
-    AD_PAGE_MODULO=$(($ADS_PDF_PAGES - 1))
-    if [ "$AD_PAGE_MODULO" -eq "0" ]; then
-        PAGES+=(1);
-    else
-        PAGES+=($(($k % $AD_PAGE + 1)));
-    fi
+    PAGES+=($ADS_PDF_PAGE);
 done
 
-pdfjoin ${PAGES[@]} --outfile print-me.pdf --paper a5paper --rotateoversize false
+pdfjam ${PAGES[@]} --outfile /dev/stdout --paper a4paper --rotateoversize false
